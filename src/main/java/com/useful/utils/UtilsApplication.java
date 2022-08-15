5d231f21c6f60c7d50498a9f0c517aa50834893f
admin <admin>
2022-08-15 17:57:19 +0800

    start

---
+++ src/main/java/com/useful/utils/UtilsApplication.java
@@ -0,0 +1,16 @@
+package com.useful.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author admin
+ */
+@SpringBootApplication
+public class UtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/useful/utils/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.useful.utils.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author admin
+ */
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
