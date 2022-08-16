932a9a4d7c7c47bf60f68b9f2a13ecfb98aa8e13
admin <admin>
2022-08-16 09:47:18 +0800

    test

--- src/main/java/com/useful/utils/controller/TestController.java
+++ src/main/java/com/useful/utils/controller/TestController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello  good";
     }
 }
